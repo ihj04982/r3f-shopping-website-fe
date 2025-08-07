935cf26678eaa7882d33efb351221ed0aaf82349
Heejeong Lim <ihj04982@gmail.com>
2025-08-08 02:13:01 +0900

    Update Vite configuration for 3D model handling and server policies
    - Added support for .glb and .gltf assets in the build process, ensuring proper asset naming conventions.
    - Configured server headers to enforce cross-origin policies for enhanced security.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    assetsInclude: ["**/*.glb", "**/*.gltf"],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    const info = assetInfo.name.split(".");
+                    const ext = info[info.length - 1];
+                    if (/glb|gltf/i.test(ext)) {
+                        return `assets/models/[name]-[hash][extname]`;
+                    }
+                    return `assets/[name]-[hash][extname]`;
+                },
+            },
+        },
+    },
+    server: {
+        headers: {
+            "Cross-Origin-Embedder-Policy": "require-corp",
+            "Cross-Origin-Opener-Policy": "same-origin",
+        },
+    },
 });
